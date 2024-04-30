9da669b74177fa444f6623c665dfb1e528a13d0b
Lucas <lucaslba2105@gmail.com>
2024-04-30 17:06:49 -0300

    finalizado

--- cliente.py
+++ cliente.py
@@ -1,26 +1,28 @@
 import socket
 import threading
+import time
 
-def receive_message(s):
-    while True:
-        data, _ = s.recvfrom(1024)  # Buffer size is 1024 bytes
-        print(f"Received message: {data.decode()}")
-        return data
+
     
-def receive_messages(s):
+    
+    
+    
+def receive_messages(s,server_host,server_port):
     while True:
         data, _ = s.recvfrom(1024)  # Buffer size is 1024 bytes
-        print(f"Received message: {data.decode()}")
+        print(f"Mensagem Recebida: {data.decode()}")
+        if data.decode() == "O jogo comecou.":
+            answerQuestions(s,server_host,server_port)
         
 def answerQuestions(s,server_host,server_port):
-    i=0
-    while(receive_message(s)!="END"):
-        answer = input("\n>> ")
-        answers=[]
-        answers.append(answer)
-        i+=1
-    message=''.join(answers)
+    nome = input("\nNome>> ") #"Nome:","Cidade:","Pais:","Animal:","Objeto:"
+    cidade = input("Cidade>>")
+    pais = input("País>>")
+    animal = input("Animal>>")
+    objeto = input("Objeto>>")
+    message = f"{nome},{cidade},{pais},{animal},{objeto}"
     send_message(s, server_host, server_port,message)
+    receive_messages(s,server_host,server_port)
     
 def send_message(s, server_host, server_port,message):
         s.sendto(message.encode(), (server_host, server_port))
@@ -30,8 +32,8 @@ def udp_client(server_host='127.0.0.1', server_port=12345):
         # Envia uma mensagem inicial para registrar o cliente no servidor
         initial_message = "Cliente conectado!"
         s.sendto(initial_message.encode(), (server_host, server_port))
-        threading.Thread(target=receive_messages, args=(s,)).start()
-        answerQuestions(s, server_host, server_port)
+        receive_messages(s,server_host,server_port)
+        
 
 if __name__ == "__main__":
     udp_client()
--- servidor.py
+++ servidor.py
@@ -8,10 +8,11 @@ class Player:
     def __init__(self,client_addrs) -> None:
         self.addres=client_addrs
         self.answer=[]
+        self.points=0
         
         
-    def setAnswers(playersAnswers,answer):
-        self.playersAnswers.append(answer)
+    def setAnswers(self,answer):
+        self.answer.append(answer)
 class Room:
     def __init__(self, capacity,letter, players,clients_addr) -> None:
         self.capacity = capacity
@@ -26,63 +27,82 @@ class Room:
     
     
     
-def startGame(s,room,start):
-    gameStartedMessage="Game has been started\nQuestions:"
+def startGame(s,room):
+    gameStartedMessage="O jogo comecou."
     send_messages(s,room.clients_addr,gameStartedMessage)
-    for question in room.playersQuestions:
-        send_messages(s,room.clients_addr,question)
-    while(time.perf_counter()-start!=60):
-        receive_messages(s,room)
-    endOfGame="END"
-    send_messages(s,room.clients_addr,endOfGame)
+    receive_answers(s,room)
+    
         
         
-def receive_messages(s,room):
-    answer, addr = s.recvfrom(1024)  # Buffer size is 1024 bytes
+def receive_answers(s,room):
+    cont = 0
+    while cont<room.capacity:
+        data, addr = s.recvfrom(1024)  # Buffer size is 1024 bytes
+        for player in room.players:
+            if addr == player.addres:
+                answers=[]
+                answers = data.decode().split(',')
+                for answer in answers:
+                    player.setAnswers(answer)
+                cont += 1
+    validate_answers(s,room)
+
+def validate_answers(s,room):
     for player in room.players:
-        if addr == room.players.addres:
-            room.players.setAnswers(answer)
+        for answer in player.answer:
+            if(answer==room.letter):
+                player.points+=10
+    send_answers(s,room)
+    
+           
+def send_answers(s,room):
+    for player in room.players:
+        pontuacao = f"Sua pontuacao foi {player.points}."
+        s.sendto(pontuacao.encode(),player.addres)
+        
 
 def send_messages(s, clients_addr,message):
     for addr in clients_addr:
         s.sendto(message.encode(), addr)
         
 def send_connection(s,client_addr):
-    message="You're connected to the game.\n"
+    message="Voce se conectou ao jogo.\n"
     s.sendto(message.encode(), client_addr)
     
     
 def sortLetter(s, clients_addr):
     letters = string.ascii_uppercase
     letter = random.choice(letters)
-    message = f'The sorted letter is {letter}\n'
+    message = f'A letra sorteada e {letter}.\n'
     for addr in clients_addr:
         s.sendto(message.encode(), addr)
     return letter
     
 def udp_serverInit(host='127.0.0.1', port=12345):
-    capacity = 1
+    capacity = 2
     players = []
     clients_addr =[]
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.bind((host, port))
         connected=0
         while(connected!=capacity):
-            print(f"Waiting for Players:{capacity-connected}")
+            aguardandoPlayers= f'Esperando por jogadores:{capacity-connected}.'
+            send_messages(s,clients_addr,aguardandoPlayers)
+            print(aguardandoPlayers)
             data, addr = s.recvfrom(1024)  # Aguarda a primeira mensagem para obter o endereço do cliente
             send_connection(s,addr)
             player= Player(addr)
             players.append(player)
             clients_addr.append(addr)
             connected+=1
-            print(f"Player has Connected\n")
-        initialMessage = 'Sorting letter...\n'
+            print(f"{data.decode()}\n")
+            
+        initialMessage = 'Sorteando letra...\n'
         send_messages(s,clients_addr,initialMessage)
         time.sleep(3)
         letter = sortLetter(s,clients_addr)
-        room = Room(capacity,players,letter,clients_addr)
-        time.sleep(5)
-        startGame(s,room,time.perf_counter())
+        room = Room(capacity,letter,players,clients_addr)
+        startGame(s,room)
         #threading.Thread(target=receive_messages, args=(s,)).start()
         
         
